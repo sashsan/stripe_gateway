6bd3ced4527d71b9104c83d02283288ca2853f23
Alex Batashov <a.batashov@gpsolutions.com>
2023-02-21 14:02:25 +0300

    Created products.

--- Gemfile
+++ Gemfile
@@ -7,20 +7,24 @@ ruby '3.2.1'
 
 gem 'bootsnap', require: false
 gem 'cssbundling-rails'
+gem 'httparty'
 gem 'jbuilder'
 gem 'jsbundling-rails'
+gem 'net-http'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
 gem 'redis', '~> 4.0'
 gem 'sprockets-rails'
 gem 'stimulus-rails'
+gem 'stripe'
 gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'pry'
+  gem 'rspec-rails', '~> 6.0.0'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+[
+  { name: 'Iphone X', price: 900 },
+  { name: 'Samsung Z Flip 3', price: 1100 },
+  { name: 'Samsung S22 Ultra', price: 1000 },
+  { name: 'Samsung Z Fold 4', price: 1500 }
+].each do |k|
+  Product.create(k)
+end
